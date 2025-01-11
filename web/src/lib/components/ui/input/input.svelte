86e22b5fa07c6c4a42989dc4992049396b55ef0e
rtrampox <isaquewrodrigues@icloud.com>
2025-01-11 03:24:48 -0300

    feat: add password visibility toggle to input component

--- web/src/lib/components/ui/input/input.svelte
+++ web/src/lib/components/ui/input/input.svelte
@@ -2,20 +2,42 @@
 	import type { HTMLInputAttributes } from "svelte/elements";
 	import type { WithElementRef } from "bits-ui";
 	import { cn } from "$lib/utils.js";
+	import { EyeIcon, EyeOffIcon } from "lucide-svelte";
 
 	let {
 		ref = $bindable(null),
 		value = $bindable(),
 		class: className,
+		type,
 		...restProps
 	}: WithElementRef<HTMLInputAttributes> = $props();
+
+	let isVisible = $state(false);
 </script>
 
-<input
-	bind:this={ref}
-	class={cn(
-		"flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-		className,
-	)}
-	bind:value
-	{...restProps} />
+<div class="relative">
+	<input
+		bind:this={ref}
+		class={cn(
+			type === "password" ? "pe-10" : "",
+			"border-input bg-background ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring flex h-10 w-full rounded-md border px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+			className,
+		)}
+		type={type === "password" ? (isVisible ? "text" : "password") : type}
+		bind:value
+		{...restProps}
+	/>
+	{#if type === "password"}
+		<button
+			class="text-muted-foreground absolute right-3 top-1/2 -translate-y-1/2 transform"
+			type="button"
+			onclick={() => (isVisible = !isVisible)}
+		>
+			{#if isVisible}
+				<EyeIcon className="size-3" />
+			{:else}
+				<EyeOffIcon className="size-3" />
+			{/if}
+		</button>
+	{/if}
+</div>
