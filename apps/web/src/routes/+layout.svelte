2340c257fad50d08d39626223bc88f18c0563cd2
rtrampox <isaquewrodrigues@icloud.com>
2025-03-01 23:31:16 -0300

    refactor: remove unused font links from layout

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -6,16 +6,6 @@
 	import "../app.css";
 </script>
 
-<svelte:head>
-	<link rel="preconnect" href="https://fonts.googleapis.com" />
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
-
-	<link
-		rel="stylesheet"
-		href="https://fonts.googleapis.com/css2?family=Geist+Mono:wght@100..900&family=Geist:wght@100..900&display=swap"
-	/>
-</svelte:head>
-
 <!-- svelte-ignore -->
 <ModeWatcher />
 {@render children()}
